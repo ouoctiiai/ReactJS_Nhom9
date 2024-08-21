e280aae5dbc0326360761fd7a521ceb9ae1c5aa6
nguyenlephong2003 <135304732+nguyenlephong2003@users.noreply.github.com>
2024-08-21 20:46:52 +0700

    add profile

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,46 @@
-import React, { useEffect } from 'react'
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useEffect, useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
 import './index.css';
-import './App.css'
+import './App.css';
 import Header from './Components/Header';
-import Footer from './Components/Footer/Foot'
+import Footer from './Components/Footer/Foot';
 import MainBody from './Components/MainBody/MainBody';
 import { Route, Routes } from 'react-router-dom';
 import Navbar from './Components/Navbar';
 import FilterQuestion from './Components/FilterQuestion';
+import Admin from './Components/Admin/Admin'; // Import Admin component
 
 function App() {
-  const [posts, setPosts] = useState([])
+  const [posts, setPosts] = useState([]);
   const [searchQuery, setSearchQuery] = useState('');
 
   useEffect(() => {
     fetch("https://66c075a5ba6f27ca9a56aed0.mockapi.io/questions")
       .then(response => response.json())
-      .then(data => setPosts(data))
-  }, [])
-  const handleSearch = (query) =>{
+      .then(data => setPosts(data));
+  }, []);
+
+  const handleSearch = (query) => {
     setSearchQuery(query);
-  }
+  };
+
   return (
     <>  
-      <div >
-        <Header onSearch = {handleSearch}/>
+      <div>
+        <Header onSearch={handleSearch} />
         <Navbar />
-        <div className=" bg-[url('./image/bg.jpg')]  bg-cover bg-center">
+        <div className=" bg-cover bg-center">
           <Routes>
-            <Route path="/filterquestion" element={<FilterQuestion questions={posts}/>} />
-             <Route path="/" element={<MainBody posts={posts} searchQuery={searchQuery} />} /> 
+            <Route path="/filterquestion" element={<FilterQuestion questions={posts} />} />
+            <Route path="/admin" element={<Admin />} /> {/* Route mới cho Admin */}
+            <Route path="/" element={<MainBody posts={posts} searchQuery={searchQuery} />} /> 
           </Routes>
         </div>
-        
         <Footer />
       </div>  
     </>
-  )
+  );
 }
-export default App
 
+export default App;
---
+++ src/Components/Admin/Admin.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Content from './components/Content'
+
+const Admin = () => {
+  return (
+    <div className="admin-container">
+      <Content />
+    </div>
+  );
+};
+
+export default Admin;
