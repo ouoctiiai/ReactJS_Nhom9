2ebeb1f328d5dc3a7bd2e48bd9ccdce772f008f7
ouoctiiai <131683324+ouoctiiai@users.noreply.github.com>
2024-08-21 22:46:12 +0700

    conflict handling

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import { Route, Routes } from 'react-router-dom';
 import Navbar from './Components/Navbar';
 import FilterQuestion from './Components/FilterQuestion';
 import Login from './Components/Signin-Login/Login';
-import Admin from './Components/Admin/Admin'; 
+import Admin from './Components/Admin/Admin'; // Import Admin component
 
 function App() {
   const [posts, setPosts] = useState([]);
@@ -19,22 +19,21 @@ function App() {
   useEffect(() => {
     fetch("https://66c075a5ba6f27ca9a56aed0.mockapi.io/questions")
       .then(response => response.json())
-      .then(data => setPosts(data));
-  }, []);
-
+      .then(data => setPosts(data))
+  }, [])
   const handleSearch = (query) => {
     setSearchQuery(query);
   };
 
   return (
-    <>  
-      <div>
+    <>
+      <div >
         <Header onSearch={handleSearch} />
         <Navbar />
         <div className=" bg-cover bg-center">
           <Routes>
             <Route path="/filterquestion" element={<FilterQuestion questions={posts} />} />
-            <Route path="/admin" element={<Admin />} />
+            <Route path="/admin" element={<Admin />} /> 
             <Route path="/" element={<MainBody posts={posts} searchQuery={searchQuery} />} />
             <Route path="/login" element={<Login/>}/>
           </Routes>
